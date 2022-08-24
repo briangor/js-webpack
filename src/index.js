90e93790936028f1f2c1c19bc7eaa0c27c71942b
briangor <briangor13@gmail.com>
2022-08-24 15:58:30 +0300

    add babel loader

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let h1 = document.querySelector('h1');
+h1.innerHTML = 'Welcome to Webpack. Happy Coding!';
+const studentName = 'Kylo';
+console.log(studentName);
+console.log('hello_ ');
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+const config = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    //watch: true,
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        hot: true,
+        //port: 9002,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+
+
+}
+
+module.exports = () => {
+    return config;
+};
